c17041477b4fbc1e19c1f006ea2c9966c80f1507
Sven A Robbestad <robbestad@gmail.com>
2016-10-04 12:15:25 +0200

    Removed things I don't want to commit

--- service-worker/serviceworker.js
+++
@@ -1,6 +0,0 @@
-self.addEventListener('install', e => {
-  console.info('Service Worker: Installed');
-});
-self.addEventListener('activate', e => {
-  console.info('Service Worker: Active');
-});
--- web-worker/worker.js
+++
@@ -1,7 +0,0 @@
-console.time('compute');
-postMessage(function(n){
-  var i = 0;
-  while (++i < n * n) {}
-  console.timeEnd('compute');
-  return i;
-}(123456));
